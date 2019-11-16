8171199fc1d38fee02a9697ea58c4f744d3ae53f
issamYahiaoui <ei_yahiaoui@esi.dz>
2019-11-16 12:59:56 +0100

    Fixes and add chatbot ...

--- App/Containers/LandingScreen.js
+++ App/Containers/LandingScreen.js
@@ -2,51 +2,103 @@ import React, { Component } from 'react'
 import {View, Text, Button,  AsyncStorage} from 'react-native';
 
 
-import SettingsActions from '../Redux/SettingsRedux'
 
 
-import { connect } from 'react-redux'
+
 import styles from './Styles/LandingScreenStyles';
-import {getLocale, setLocale, t} from '../Services/I18n';
-// Styles
+import colors from '../Themes/Colors'
+import ChatBot from "../Components/react-native-chatbot/lib";
 
+
+// Styles
+const parks = [
+    {
+        id : 1 ,
+        title : 'Park 1'
+    },
+    {
+        id : 2 ,
+        title : 'Park 2'
+    },
+    {
+        id : 3 ,
+        title : 'Park 3'
+    }
+]
+
+
+const steps = [
+    {
+        id: '1',
+        message: 'Hello, Where are you going?',
+        trigger: '4',
+    },
+
+    {
+        id: '3',
+        message: "Awesome!  Could you tell us please when ?",
+        trigger: '4',
+    },
+    {
+        id: '4',
+        options: [
+            { value: 1, component: (<View><Text>Hello</Text><Text> :: There</Text></View>), trigger: '5' },
+            { value: 2, label: 'Some other date', trigger: '5' },
+            { value: 3, label: 'Some other date ...', trigger: '5' },
+        ],
+    },
+    {
+        id: '5',
+        message: "Awesome!  So Now we wanna know what are you planning to do so that we can guide you ? ",
+        trigger: '6',
+    },
+    {
+        id: '6',
+        options: [
+            { value: 1, label: 'Family Trip', trigger: '7' },
+            { value: 2, label: 'Sports', trigger: '7' },
+            { value: 3, label: 'Camping', trigger: '7' },
+            { value: 4, label: 'Exploring', trigger: '7' },
+            { value: 4, label: 'Optional from us !', trigger: '7' },
+        ],
+    },
+    {
+        id: '7',
+        message: "Great!  You are good to go ! Let the fun begin ",
+        end : true
+    }
+];
 class LandingScreen extends Component {
 
     state = {
 
     }
 
+    static navigationOptions = {
+        title: 'Welcome to the chatbot!',
+    };
+
+
+
 
     render() {
+
+
+
         return (
-            <View>
-                <Button title="Show me more of the app" onPress={this._showMoreApp} />
-                <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
-            </View>
+
+
+            <ChatBot
+                style={{backgroundColor : colors.primary , color : colors.primary}}
+                steps={steps} />
+
+
         );
     }
 
-    _showMoreApp = () => {
-        this.props.navigation.navigate('PhoneAuth');
-    };
-
-    _signOutAsync = async () => {
-        await AsyncStorage.clear();
-        this.props.navigation.navigate('Auth');
-    };
 }
 
-const mapStateToProps = ({settings}) => {
-    return {
-        lang:settings.lang
-    }
-}
 
-const mapDispatchToProps = (dispatch) => {
-    return {
 
-        changeLanguage: (lang) => dispatch(SettingsActions.changeLanguage(lang)),
-    }
-}
+export default LandingScreen
 
-export default connect(mapStateToProps, mapDispatchToProps)(LandingScreen)
